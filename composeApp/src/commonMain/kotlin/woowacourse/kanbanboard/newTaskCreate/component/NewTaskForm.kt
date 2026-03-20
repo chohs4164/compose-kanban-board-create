45db5860da36cf50b532fd7683d43c11914ebfe3
chohs4164 <josh100as@gmail.com>
2026-03-20 21:23:09 +0900

    feat: 배경색 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanbanboard/newTaskCreate/component/NewTaskForm.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanbanboard/newTaskCreate/component/NewTaskForm.kt
@@ -1,5 +1,6 @@
 package woowacourse.kanbanboard.newTaskCreate.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,6 +33,7 @@ fun NewTaskForm(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .background(Color.White)
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(24.dp),
