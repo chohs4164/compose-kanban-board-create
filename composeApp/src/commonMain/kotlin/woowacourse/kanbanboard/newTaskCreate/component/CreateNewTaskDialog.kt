18e6dbcdd9080aed7b637eee29e8e6ce16d8c343
chohs4164 <josh100as@gmail.com>
2026-03-20 21:16:41 +0900

    refactor: CreateNewTaskDialog 컴포넌트 크기 고정 및 색상 부여

--- composeApp/src/commonMain/kotlin/woowacourse/kanbanboard/newTaskCreate/component/CreateNewTaskDialog.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanbanboard/newTaskCreate/component/CreateNewTaskDialog.kt
@@ -1,23 +1,36 @@
 package woowacourse.kanbanboard.newTaskCreate.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.kanbanboard.newTaskCreate.uiState.rememberCreateTaskFormState
 
 @Composable
-fun CreateNewTaskDialog() {
-    val formState =
-        rememberCreateTaskFormState()
+fun CreateNewTaskDialog(onDismiss: () -> Unit) {
+    val formState = rememberCreateTaskFormState()
 
-    Column {
+    Column(
+        modifier = Modifier
+            .size(672.dp, 818.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.White),
+    ) {
         // 상단부
-        TopBar()
+        TopBar(onDismiss)
         HorizontalDivider()
         // 중단부
         NewTaskForm(
@@ -36,15 +49,15 @@ fun CreateNewTaskDialog() {
         // 하단부
         Box(modifier = Modifier.padding(vertical = 16.dp)) {
             CreateNewTaskDialogBottom(
-                isCreateEnabled = formState.isCreateEnabled
+                isCreateEnabled = formState.isCreateEnabled,
             )
         }
     }
 }
 
-@Preview(widthDp = 672, heightDp = 1000)
+@Preview(widthDp = 672, heightDp = 1000, showBackground = true)
 @Composable
 fun CreateNewTaskDialogPreview(
 ) {
-    CreateNewTaskDialog()
+    CreateNewTaskDialog(onDismiss = {})
 }
